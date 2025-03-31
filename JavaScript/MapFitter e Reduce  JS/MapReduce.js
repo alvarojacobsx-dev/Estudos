12450a2ed400aadcf9711801954181580922cd90
alvarojacobson <alvarojacobsx@gmail.com>
2025-03-31 10:05:14 -0300

    Update

--- JavaScript/MapFitter e Reduce  JS/MapReduce.js
+++ JavaScript/MapFitter e Reduce  JS/MapReduce.js
@@ -4,22 +4,22 @@ const distanciaEmMetros = [1000, 1500, 2000, 2500, 3000, 3500, 4000]
 // MAP 
 
 const distanciaEmKm = distanciaEmMetros.map(distancia => distancia / 1000);
-console.log ("Distancia em KM: ", distanciaEmKm)
+console.log ("Distancia em KM: ", distanciaEmKm);
 
 
 // FILTER
 
 const intensFiltrados = distanciaEmMetros.filter ((distancia) => {
     if (distancia > 2000) return distancia;
-})
+});
 
-console.log("Intens filtrados: ", intensFiltrados)
+console.log("Intens filtrados: ", intensFiltrados);
 
 
 // REDUCE
 
 const distanciaTotal = distanciaEmMetros.reduce((acc, distancia) => {
     return acc + distancia 
-}, 0)
+}, 0);
 
-console.log("Distancia Total ", distanciaTotal  )
+console.log("Distancia Total ", distanciaTotal);
